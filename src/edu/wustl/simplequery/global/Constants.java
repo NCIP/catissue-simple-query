85274d137c887660f4db073d1f3b995725e17f8f
Deepti Shelar <deepti_shelar>
2008-11-28 06:59:03 +0000

    Moving SimpleQuery constants to SimpleQuery project
    SVN-Revision: 107

---
+++ src/edu/wustl/simplequery/global/Constants.java
@@ -0,0 +1,33 @@
+package edu.wustl.simplequery.global;
+
+/**
+ * Constants required for SimpleQuery.
+ * @author deepti_shelar
+ *
+ */
+public class Constants
+{
+	public static final String FIELD_TYPE_BIGINT = "bigint";
+	public static final String FIELD_TYPE_VARCHAR = "varchar";
+	public static final String FIELD_TYPE_TEXT = "text";
+	public static final String FIELD_TYPE_TINY_INT = "tinyint";
+	public static final String NULL = "NULL";
+
+	public static final String CONDITION_VALUE_YES = "yes";
+
+	public static final String TINY_INT_VALUE_ONE = "1";
+	public static final String TINY_INT_VALUE_ZERO = "0";
+	public static final String PARENT_SPECIMEN_ID_COLUMN = "PARENT_SPECIMEN_ID";
+	public static final int SIMPLE_QUERY_INTERFACE_ID = 40;
+	public static final String SIMPLE_QUERY_MAP = "simpleQueryMap";
+	public static final String SIMPLE_QUERY_ALIAS_NAME = "simpleQueryAliasName";
+
+	public static final String SIMPLE_QUERY_INTERFACE_ACTION = "/SimpleQueryInterface.do";
+	public static final String HYPERLINK_COLUMN_MAP = "hyperlinkColumnMap";
+	public static final String TABLE_ALIAS_NAME = "aliasName";
+	public static final String ATTRIBUTE_NAME_LIST = "attributeNameList";
+	public static final String ATTRIBUTE_CONDITION_LIST = "attributeConditionList";
+	public static final String ORIGINAL_SIMPLE_QUERY_OBJECT = "originalSimpleQueryObject";
+	public static final String SIMPLE_QUERY_COUNTER = "counter";
+
+}
