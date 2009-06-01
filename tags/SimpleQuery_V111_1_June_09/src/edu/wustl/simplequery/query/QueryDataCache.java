6815c8411807c7799780bc2956898befd410ec7d
Kalpana Thakur <kalpana_thakur>
2009-06-01 05:54:50 +0000

    Tagged Simple Query with latest cp and washu_commons.jar
    SVN-Revision: 181

---
+++ tags/SimpleQuery_V111_1_June_09/src/edu/wustl/simplequery/query/QueryDataCache.java
@@ -0,0 +1,56 @@
+/**
+ * <p>Title: QueryDataCache Class>
+ * <p>Description:  This Class is used to cache all the data (table names, table display names,
+ *  column names, column display name etc.) required for query modules.</p>
+ * Copyright:    Copyright (c) year
+ * Company: Washington University, School of Medicine, St. Louis.
+ * @author Aniruddha Phadnis
+ * @version 1.00
+ * Created on Nov 22, 2005
+ */
+
+package edu.wustl.simplequery.query;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import edu.wustl.common.util.global.CommonServiceLocator;
+import edu.wustl.dao.DAO;
+import edu.wustl.dao.daofactory.DAOConfigFactory;
+import edu.wustl.dao.daofactory.IDAOFactory;
+import edu.wustl.dao.exception.DAOException;
+
+/**
+ * This Class is used to cache all the data (table names, table display names,
+ * column names, column display name etc.) required for query modules.
+ * @author aniruddha_phadnis
+ */
+public class QueryDataCache
+{
+
+	/**
+	 * Returns a map of all query data.
+	 * @return a map of all query data.
+	 */
+	public static Map getQueryData() throws DAOException
+	{
+		String appName=CommonServiceLocator.getInstance().getAppName();
+		IDAOFactory daofactory = DAOConfigFactory.getInstance().getDAOFactory(appName);
+		DAO dao = daofactory.getDAO();
+		
+
+		dao.openSession(null);
+		List list = dao.retrieve(QueryTableData.class.getName());
+		dao.closeSession();
+
+		HashMap tableMap = new HashMap();
+
+		for (int i = 0; i < list.size(); i++)
+		{
+			QueryTableData tableData = (QueryTableData) list.get(i);
+			tableMap.put(tableData.getAliasName(), tableData);
+		}
+		return tableMap;
+	}
+}
